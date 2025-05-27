d0e1b45aa51787e018088fe5c06e1f607f67e1d0
Derlie Monne <derlie.monne@gmail.com>
2025-05-27 11:02:40 +1000

    Update Record.cs

--- Assets/Scripts/Record.cs
+++ Assets/Scripts/Record.cs
@@ -5,5 +5,4 @@ public struct Record
     public string Name;
     public int Place;
     public int MaxAltitude;
-    // public int CompareTo(Record obj) => Place.CompareTo(obj.MaxAltitude); : IComparable<Record>
 }
